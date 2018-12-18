c7648751bc2c66d8e0570259e36f1659ecf465f0
Oğuzhan Türker <oguzturker8@gmail.com>
2018-12-18 13:46:44 +0300

    Update obss_1.py

--- obss_1.py
+++ obss_1.py
@@ -1,25 +1,25 @@
 def IsSorted(A):
-    error = 0;
-    statement = True;
+    error = 0
+    statement = True
     
     for i in range(len(A)-1):
-        if(A[i] <= A[i+1]):
-           continue;
+        if A[i] <= A[i+1]:
+           continue
         else:
-           error += 1;
-    if(error >= 2):
-       return 0;
-    elif(error == 0):
-       statement = True;
-       return statement;
+           error += 1
+    if error >= 2:
+       return 0
+    elif error == 0:
+       #statement = True
+       return len(A)
     else:
-       count = find(A);
-       return count;
+       count = find(A)
+       return count
     
 def find(A):
-    count = 0;
+    count = 0
     for i in range(len(A)-1):
-        tmp = [x for x in A if x != A[i]];
-        if(IsSorted(tmp) == True):
-            count += 1;
-    return count;
+        tmp = [x for x in A if x != A[i]]
+        if IsSorted(tmp) == len(tmp):
+            count += 1
+    return count
